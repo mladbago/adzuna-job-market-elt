b9112d7ba2ea536e4b8b9e5476374efc2abdd0b6
Blagoja Mladenov <mladenovblagoja@gmail.com>
2026-03-26 00:34:59 +0100

    feat: Implemented a testing solution for reading data from API and sending slack notification on fail and success - Implemented data fetching from the API inside Airflow - Implemented Slack notifications on pipeline outcome using Slack Webhooks - Tested both failure and success scenarios for the alerting

--- airflow/dags/my_comsos_dag.py
+++ airflow/dags/my_comsos_dag.py
@@ -1,13 +1,26 @@
 from datetime import datetime
 from pathlib import Path
-from airflow.decorators import dag
+from airflow.sdk import dag, task 
 from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, RenderConfig, ExecutionConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
-
+import os 
+import requests
+from airflow.providers.slack.notifications.slack_webhook import SlackWebhookNotifier
 
 DBT_PROJECT_PATH = Path("/usr/local/airflow/dags/dbt_project")
 
 DBT_EXECUTABLE_PATH = "/usr/local/airflow/dbt_venv/bin/dbt"
+
+
+on_failure_notifier = SlackWebhookNotifier(
+    slack_webhook_conn_id="slack_conn",
+    text="❌ *DAG Failed!* \n*DAG:* {{ dag.dag_id }} \n*Execution Time:* {{ dag_run.logical_date }}"
+)
+
+on_success_notifier = SlackWebhookNotifier(
+    slack_webhook_conn_id="slack_conn",
+    text="✅ *DAG Succeeded!* \n*Project:* Adzuna ELT Pipeline \n*Status:* All data moved to Snowflake."
+)
 
 profile_config = ProfileConfig(
     profile_name="adzuna_test",
@@ -22,6 +35,8 @@ profile_config = ProfileConfig(
     start_date=datetime(2026, 3, 25),
     catchup=False,
     tags=['test', 'dbt'],
+    on_failure_callback=[on_failure_notifier],
+    on_success_callback=[on_success_notifier]
 )
 def test_dbt_standalone():
     
@@ -36,5 +51,33 @@ def test_dbt_standalone():
             select=["path:models"], 
         ),
     )
+
+    @task
+    def fetch_adzuna_jobs(page: int = 1): 
+        APP_ID = os.getenv("ADZUNA_APP_ID")
+        APP_KEY = os.getenv("ADZUNA_APP_KEY")
+        BASE_URL = os.getenv("BASE_URL")
+
+        if not APP_ID or not APP_KEY: 
+            raise ValueError("Adzuna API credentials are not set in environment variables.")
+        
+        base_url = f"https://api.adzuna.com/v1/api/jobs/pl/search/{page}"
+
+        params = {
+            "app_id": APP_ID,
+            "app_key": APP_KEY,
+            "results_per_page": 50,
+            "max_days_old": 1,
+            "content-type": "application/json"
+        }
+
+        response = requests.get(base_url, params=params)
+        response.raise_for_status() 
+        data = response.json()
+
+        found_count = len(data.get('results',[]))
+        print(f"Successfully retrieved {found_count} job listings.")
+        return data 
+    fetch_adzuna_jobs() >> dbt_test_run
 
 test_dbt_standalone()
